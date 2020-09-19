d05f7da4ac35872fa680f08669de072549bedeeb
PyBudda <walvater.wotan7@gmail.com>
2020-09-19 18:58:09 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+from difflib import get_close_matches
+
+
+data = json.load(open("data\\data.json"))
+
+
+def translate(some_word):
+    some_word = some_word.lower()
+    some_word = get_close_matches(some_word, data.keys())[0]
+    if some_word in data:
+        return data[some_word]
+    else:
+        return("The word doesn't exist")
+
+
+word = input("Enter a word: ")
+
+print(translate(word))
