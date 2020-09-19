564f87a7194ef689146b57ade2c993554f2d9c19
PyBudda <44323133+MishaRusalimov@users.noreply.github.com>
2020-09-20 00:01:32 +0300

    Make a better output. Lesson 093

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ data = json.load(open("data\\data.json"))
 
 def translate(some_word):
     some_word = some_word.lower()
-    some_word = get_close_matches(some_word, data.keys())[0]
     if some_word in data:
         return data[some_word]
+    elif len(get_close_matches(some_word, data.keys(), cutoff=0.8)) > 0:
+        return"Did you mean %s instead?" % get_close_matches(some_word, data.keys())[0]
     else:
         return("The word doesn't exist")
 
