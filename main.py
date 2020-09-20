22752847de765f94ecfc34bc26b7367a587bc69e
PyBudda <44323133+MishaRusalimov@users.noreply.github.com>
2020-09-20 13:45:27 +0300

    More userfriendly interface.

--- main.py
+++ main.py
@@ -12,10 +12,18 @@ def translate(some_word):
     some_word = some_word.lower()
     if some_word in data:
         return data[some_word]
-    elif len(get_close_matches(some_word, data.keys()) > 0:
-        return"Did you mean %s instead?" % get_close_matches(some_word, data.keys())[0]
+    elif len(get_close_matches(some_word, data.keys())) > 0:
+        match_words = get_close_matches(some_word, data.keys())
+        yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % match_words[0])
+        yn = yn.lower()
+        if yn == 'y':
+            return data[match_words[0]]
+        elif yn == 'n':
+            return "The word doesn't exist!"
+        else:
+            return "I don't understand you!"
     else:
-        return("The word doesn't exist")
+        return "The word doesn't exist"
 
 
 word = input("Enter a word: ")
