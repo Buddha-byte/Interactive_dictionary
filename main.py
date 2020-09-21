cf261d5c9d5e22df04f8897bac11b54d08476bf9
44323133+MishaRusalimov@users.noreply.github.com <44323133+MishaRusalimov@users.noreply.github.com>
2020-09-21 23:09:15 +0300

    Decision to make a new branch

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def translate(some_word):
     some_word = some_word.lower()
 
     con = mysql.connector.connect(
-    user = "ardit700_student",
-    password="ardit700_student",
-    host = "108.167.140.122",
-    database = "ardit700_pm1database"
+        user="ardit700_student",
+        password="ardit700_student",
+        host="108.167.140.122",
+        database="ardit700_pm1database"
     )
 
     cursor = con.cursor()
@@ -24,20 +24,23 @@ def translate(some_word):
     query = cursor.execute("SELECT * FROM Dictionary WHERE Expression='%s'" % some_word)
     data = cursor.fetchall()
 
-    if data:
-        for word in data:
-            print(word)
-    else:
-        return "The word doesn't exist"
+    for words_tuple in data:
+        if some_word == words_tuple[0]:
+            return data
+        else:
+            if len(get_close_matches(some_word, words_tuple[0])) > 0:
+                yn = input("Did you mean: %s? print Y for yes, or N for no" % get_close_matches(some_word, data[0]))
+                yn.lower()
+                if yn == "y":
+                    for word in data:
+                        print(word[1])
+                        time.sleep(5)
+                elif yn == "n":
+                    return "The word doesn't exist"
+                else:
+                    return "I don't understand you!"
 
 
 word = input("Enter a word: ")
 
 output = translate(word)
-
-if type(output) == list:
-    for item in output:
-        print(item)
-        time.sleep(3)
-else:
-    print(output)
