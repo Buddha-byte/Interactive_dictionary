b4866bff98cdc805d75717d3991dde34951ac0fe
44323133+MishaRusalimov@users.noreply.github.com <44323133+MishaRusalimov@users.noreply.github.com>
2020-09-21 12:45:55 +0300

    Add a database

--- main.py
+++ main.py
@@ -4,25 +4,29 @@
 import json
 from difflib import get_close_matches
 import time
+import mysql.connector
 
 
-data = json.load(open("data//data.json"))
 
 
 def translate(some_word):
     some_word = some_word.lower()
-    if some_word in data:
-        return data[some_word]
-    elif len(get_close_matches(some_word, data.keys())) > 0:
-        match_words = get_close_matches(some_word, data.keys())
-        yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % match_words[0])
-        yn = yn.lower()
-        if yn == 'y':
-            return data[match_words[0]]
-        elif yn == 'n':
-            return "The word doesn't exist!"
-        else:
-            return "I don't understand you!"
+
+    con = mysql.connector.connect(
+    user = "ardit700_student",
+    password="ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+    )
+
+    cursor = con.cursor()
+
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression='%s'" % some_word)
+    data = cursor.fetchall()
+
+    if data:
+        for word in data:
+            print(word)
     else:
         return "The word doesn't exist"
 
